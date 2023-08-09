3d2760e520fbd64fa891e22c0da465363c2f52a9
Dale Hendricks <xperimance@gmail.com>
2023-08-09 18:44:30 +0900

    UI and JS Update

--- scripts.js
+++ scripts.js
@@ -2,15 +2,17 @@ const container = document.querySelector('.container');
 const buttonElement = document.createElement('button');
 buttonElement.textContent = 'Ask Your Crush!';
 buttonElement.addEventListener('click', function() {
+    const userName = prompt("What's your name?")
     const crushName = prompt("What's your crush's name?");
 
     if (crushName !== null) { // If user clicks 'cancel'
         const confirmed = confirm(`Are you sure you want to ask ${crushName} to go out with you?`);
-
         let response = '';
         if (confirmed) {
-            response = prompt(`${crushName}, will you go out with me? (Yes/No)`);
+            response = prompt(`${crushName}, will you go out with ${userName}? (Yes/No)`);
         }
+
+        const instruction = confirm(`Please pass back to ${userName}`)
 
         const message = document.createElement('p');
         if (response !== null) {
